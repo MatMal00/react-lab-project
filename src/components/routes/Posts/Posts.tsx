868f743d0d88d99e79f3c7541f3ff7024170410f
matmal00 <m.malec@bsgroup.eu>
2023-11-12 16:15:14 +0100

    feat: add svg loader

--- src/components/routes/Posts/Posts.tsx
+++ src/components/routes/Posts/Posts.tsx
@@ -1,16 +1,50 @@
 import { FC } from "react";
 import { IPost } from "src/types";
-import { useFetchPosts } from "src/libs";
-import { ActionsHandler } from "src/components";
+// import { useFetchPosts } from "src/libs";
+// import { ActionsHandler } from "src/components";
 import { PostsList } from "./components";
 import styles from "./posts.module.scss";
 
+const dummyData: IPost[] = [
+    {
+        title: "Title1",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam molestias eveniet rerum nihil amet animi iusto. Nemo illo rerum similique magnam sequi. Ipsum velit facilis dicta praesentium perspiciatis, tempore quam?",
+        id: 1,
+        userId: 2,
+    },
+    {
+        title: "Title1",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam molestias eveniet rerum nihil amet animi iusto. Nemo illo rerum similique magnam sequi. Ipsum velit facilis dicta praesentium perspiciatis, tempore quam?",
+        id: 2,
+        userId: 1,
+    },
+    {
+        title: "Title1",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam molestias eveniet rerum nihil amet animi iusto. Nemo illo rerum similique magnam sequi. Ipsum velit facilis dicta praesentium perspiciatis, tempore quam?",
+        id: 3,
+        userId: 1,
+    },
+    {
+        title: "Title1",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam molestias eveniet rerum nihil amet animi iusto. Nemo illo rerum similique magnam sequi. Ipsum velit facilis dicta praesentium perspiciatis, tempore quam?",
+        id: 4,
+        userId: 1,
+    },
+    {
+        title: "Title1",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam molestias eveniet rerum nihil amet animi iusto. Nemo illo rerum similique magnam sequi. Ipsum velit facilis dicta praesentium perspiciatis, tempore quam?",
+        id: 5,
+        userId: 1,
+    },
+];
+
 export const Posts: FC = () => {
-    const postsState = useFetchPosts();
+    // const postsState = useFetchPosts();
 
     return (
         <section className={styles.posts}>
-            <ActionsHandler<IPost[]> {...postsState}>{(posts) => <PostsList posts={posts} />}</ActionsHandler>
+            {/* <ActionsHandler<IPost[]> {...postsState}>{(posts) => <PostsList posts={posts} />}</ActionsHandler> */}
+            <PostsList posts={dummyData} />
         </section>
     );
 };
--- src/components/routes/Posts/components/PostsList/PostsList.tsx
+++ src/components/routes/Posts/components/PostsList/PostsList.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
 import { IPost } from "src/types";
+import { ReactComponent as ManAvatar } from "src/images/man-avatar.svg";
+import styles from "./PostsList.module.scss";
 
 interface IPostsListProps {
     posts: IPost[];
@@ -7,9 +9,20 @@ interface IPostsListProps {
 
 export const PostsList: FC<IPostsListProps> = ({ posts }) => {
     return (
-        <ul>
-            {posts.map((post) => (
-                <li key={post.id}>{post.title}</li>
+        <ul className={styles.list}>
+            {posts.map(({ id, title, body }) => (
+                <li className={styles.item} key={id}>
+                    <div className={styles.contentContainer}>
+                        <div>
+                            <ManAvatar />
+                        </div>
+                        <article>
+                            <h3>{title}</h3>
+                            <p>{body}</p>
+                        </article>
+                    </div>
+                    <button>comments</button>
+                </li>
             ))}
         </ul>
     );
