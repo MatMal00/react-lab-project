e45d5e19f27cf90a75989e423592ff5d46372ab7
matmal <mateusz.malec@microsoft.wsei.edu.pl>
2023-11-14 12:51:40 +0100

    fix: remove unused code

--- src/components/routes/Posts/components/PostsList/PostsList.tsx
+++ src/components/routes/Posts/components/PostsList/PostsList.tsx
@@ -1,7 +1,6 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import { IPost } from "src/types";
 import { PostListItem } from "../PostListItem";
-import { Modal } from "src/components";
 import styles from "./PostsList.module.scss";
 
 interface IPostsListProps {
@@ -9,25 +8,11 @@ interface IPostsListProps {
 }
 
 export const PostsList: FC<IPostsListProps> = ({ posts }) => {
-    const [isOpen, setIsOpen] = useState(false);
     return (
-        <>
-            <ul className={styles.list}>
-                {posts.map((post) => (
-                    <PostListItem {...post} key={post.id} />
-                ))}
-            </ul>
-            <button onClick={() => setIsOpen(true)}>openModal</button>
-            <Modal isOpen={isOpen} handleClose={() => setIsOpen(false)}>
-                <div>
-                    <h1>content</h1>
-                    <p>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum autem, aut expedita in
-                        officia deleniti. Pariatur sunt, qui illum est vitae ex quas ullam ut assumenda reiciendis
-                        tempore veniam magni!
-                    </p>
-                </div>
-            </Modal>
-        </>
+        <ul className={styles.list}>
+            {posts.map((post) => (
+                <PostListItem {...post} key={post.id} />
+            ))}
+        </ul>
     );
 };
