4bb5b420cfae5ba484a694ca037b0a6b30da74ca
matmal <mateusz.malec@microsoft.wsei.edu.pl>
2023-11-20 20:03:24 +0100

    feat: add album skeleton

--- src/components/routes/Albums/Albums.tsx
+++ src/components/routes/Albums/Albums.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import { useFetchAlbums } from "src/libs";
-import { ActionsHandler } from "src/components";
+import { ActionsHandler, Skeleton } from "src/components";
 import { IAlbum } from "src/types";
 import { AlbumsList } from "./components";
 import styles from "./Albums.module.scss";
@@ -12,7 +12,9 @@ export const Albums: FC<IAlbumsProps> = () => {
 
     return (
         <section className={styles.albums}>
-            <ActionsHandler<IAlbum[]> {...albumsState}>{(albums) => <AlbumsList albums={albums} />}</ActionsHandler>
+            <ActionsHandler<IAlbum[]> {...albumsState} skeleton={<Skeleton.Album noOfSkeletons={9} />}>
+                {(albums) => <AlbumsList albums={albums} />}
+            </ActionsHandler>
         </section>
     );
 };
