a236ce81f42350b248cd3dc7d2ba56efd4fd614f
mmalec00 <mmalec00@bitbucket.org>
2024-01-02 11:35:49 +0100

    feat: handle fetch photos

--- src/components/routes/Album/Album.tsx
+++ src/components/routes/Album/Album.tsx
@@ -1,11 +1,21 @@
 import { FC } from "react";
 import { useParams } from "react-router-dom";
+import { ActionsHandler, Skeleton } from "src/components";
+import { useFetchAlbum } from "src/libs";
+import { IPhoto } from "src/types";
+import styles from "./Album.module.scss";
 
 interface IAlbumProps {}
 
 export const Album: FC<IAlbumProps> = () => {
     const { albumId } = useParams();
-    console.log(albumId);
+    const albumState = useFetchAlbum(albumId);
 
-    return <div></div>;
+    return (
+        <section className={styles.album}>
+            <ActionsHandler<IPhoto[]> {...albumState} skeleton={<Skeleton.Album noOfSkeletons={9} />}>
+                {() => <div>hello</div>}
+            </ActionsHandler>
+        </section>
+    );
 };
--- src/libs/index.ts
+++ src/libs/index.ts
@@ -2,3 +2,4 @@ export * from "./useFetchPosts";
 export * from "./useFetchComments";
 export * from "./useFetchUserDetails";
 export * from "./useFetchAlbums";
+export * from "./useFetchAlbum";
