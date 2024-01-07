13465beba9035953b71538dd24c5a6eccf802ce0
patrykluszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2024-01-08 00:47:47 +0100

    feat: implement validation of login form

--- src/components/routes/Login/components/LoginBoxForm/LoginBoxForm.tsx
+++ src/components/routes/Login/components/LoginBoxForm/LoginBoxForm.tsx
@@ -4,14 +4,44 @@ import styles from "./LoginBoxForm.module.scss";
 import { useFetchAuth } from "src/libs";
 import Cookies from "js-cookie";
 
+interface FormErrors {
+    email?: string;
+    password?: string;
+}
+
 export const LoginBoxForm: FC = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState({});
+
     const navigate = useNavigate();
     const { login } = useFetchAuth();
+    console.log(errors);
+
+    const validateForm = () => {
+        const newErrors: FormErrors = {};
+
+        if (!email) {
+            newErrors.email = "Email is required";
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+            newErrors.email = "Email address is invalid";
+        }
+
+        if (!password) {
+            newErrors.password = "Password is required";
+        } else if (password.length < 6) {
+            newErrors.password = "Password must be at least 6 characters";
+        }
+
+        setErrors(newErrors);
+
+        return Object.keys(newErrors).length === 0;
+    };
 
     const handleLogin = async (e: MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
+
+        if (!validateForm()) return;
 
         try {
             const response = await login({ email, password });
