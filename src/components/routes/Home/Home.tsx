b9399c8aac38ebb3d3a4ed970fae726cacd2e53e
Szymon Maj <szymon.maj@futurum.tech>
2024-01-12 13:01:44 +0100

    feat: add background to home

--- src/components/routes/Home/Home.tsx
+++ src/components/routes/Home/Home.tsx
@@ -2,5 +2,9 @@ import { FC } from "react";
 import styles from "./Home.module.scss";
 
 export const Home: FC = () => {
-    return <main className={styles.home}></main>;
+    return (
+        <main className={styles.home}>
+            <div className={styles.homeBg}></div>
+        </main>
+    );
 };
