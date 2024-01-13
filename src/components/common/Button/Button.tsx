6c0b97a18ad8ba441c39c3c3bc9f9ff83f4769f3
jakub.machnica <machnicajakub@gmail.com>
2024-01-13 19:41:28 +0100

    feat: create skeleton of the button reusable component

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,9 +1,37 @@
 import { FC, ReactNode } from "react";
+import AddIcon from "@icons/add.svg";
+import RemoveIcon from "@icons/remove.svg";
+import EditIcon from "@icons/edit.svg";
+import cn from "classnames";
+import styles from "./Button.module.scss";
+
+type TActionTypeIcon = {
+    add: ReactNode;
+    remove: ReactNode;
+    edit: ReactNode;
+};
+
+const ACTION_ICON: TActionTypeIcon = {
+    add: <AddIcon />,
+    remove: <RemoveIcon />,
+    edit: <EditIcon />,
+};
 
 interface IButtonProps {
-    children: ReactNode;
+    text: string;
+    theme: "primary" | "secondary";
+    icon?: ReactNode;
+    actionType?: keyof TActionTypeIcon;
+    onClick?: () => void;
 }
 
-export const Button: FC<IButtonProps> = ({ children }) => {
-    return <button>{children}</button>;
+export const Button: FC<IButtonProps> = ({ text, icon, onClick, actionType, theme = "primary" }) => {
+    const actionIcon = actionType ? ACTION_ICON[actionType] : undefined;
+
+    return (
+        <button onClick={onClick} className={(styles.button, cn([styles[theme]]))}>
+            {(icon || actionIcon) && <span>{icon}</span>}
+            {text}
+        </button>
+    );
 };
