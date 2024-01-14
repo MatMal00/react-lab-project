b674291f11e7141f66e08c35eafc5eae27ca01d2
jakub.machnica <machnicajakub@gmail.com>
2024-01-14 18:00:54 +0100

    ref: useAuth - get rid of error state

--- src/libs/useAuth.ts
+++ src/libs/useAuth.ts
@@ -1,31 +1,25 @@
+import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import useSWRMutation from "swr/mutation";
 import useImmutableSWR from "swr/immutable";
 import { getUserFromLocalStorage, sendLoginCall } from "src/actions";
-import { useCallback, useState } from "react";
-import { apiErrorHandler } from "src/helpers";
 import { IUser } from "src/types";
 
 export const useAuth = () => {
     const navigate = useNavigate();
-    const [error, setError] = useState<string | undefined>();
     const { data: user, mutate } = useImmutableSWR<IUser | undefined>("/user", {
         fallbackData: getUserFromLocalStorage(),
     });
-    const { trigger, isMutating: isLoading } = useSWRMutation("/users", sendLoginCall);
+    const { trigger, isMutating: isLoading, error } = useSWRMutation("/users", sendLoginCall);
 
     const login = useCallback(
         async (email: string) => {
-            try {
-                const userToLogin = await trigger({ email });
+            const userToLogin = await trigger({ email });
 
-                mutate(userToLogin, { revalidate: false });
-                window.localStorage.setItem("user", JSON.stringify(userToLogin));
+            mutate(userToLogin, { revalidate: false });
+            window.localStorage.setItem("user", JSON.stringify(userToLogin));
 
-                navigate("/", { replace: true });
-            } catch (error) {
-                setError(apiErrorHandler(error));
-            }
+            navigate("/", { replace: true });
         },
         [mutate, navigate, trigger]
     );
