04b585b7789a849fc2c3a3d4bd538bc867478de7
Jakub Machnica <machnicajakub@gmail.com>
2024-01-26 20:35:51 +0100

    change formatting

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -9,11 +9,15 @@ export const Footer: FC<IFooterProps> = () => {
     const currentYear = new Date().getFullYear();
     const contactUs = "Contact Us: labproject@microsoft.wsei.edu.pl";
 
-    return <footer className={styles.footer}>
+    return (
+        <footer className={styles.footer}>
             <div className="footer-content">
                 <AppLogoIcon style={{ marginRight: "20px" }}></AppLogoIcon>
-                <p>{projectName} &copy; {currentYear}</p>
+                <p>
+                    {projectName} &copy; {currentYear}
+                </p>
                 <p>{contactUs}</p>
             </div>
-        </footer>;
+        </footer>
+    );
 };
