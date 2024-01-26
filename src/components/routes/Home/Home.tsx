62e1ab109f576cc04cae934a543bb5793dd11d51
Szymon Maj <szymon.maj@futurum.tech>
2024-01-26 14:10:09 +0100

    feat: add HeroSection

--- src/components/routes/Home/Home.tsx
+++ src/components/routes/Home/Home.tsx
@@ -1,10 +1,13 @@
 import { FC } from "react";
 import styles from "./Home.module.scss";
+import { HeroSection } from "./components/HeroSection";
 
 export const Home: FC = () => {
     return (
         <main className={styles.home}>
-            <div className={styles.backgroundImage}></div>
+            <div className={styles.backgroundImage}>
+                <HeroSection />
+            </div>
         </main>
     );
 };
